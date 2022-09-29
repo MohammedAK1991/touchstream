cea6a7a7c7d27e9161f79d377ea07b296c0c80ae
mohammedAK1991 <mohmed.ak.17@gmail.com>
2022-09-29 11:30:36 +0200

    feat(useVideosBySearchQuery): move logic to fetch vidoes baded on query into custom hook

---
+++ data/videosBySearchQuery.ts
@@ -0,0 +1,66 @@
+import { useState , useEffect} from 'react'
+import axios from 'axios'
+import dayjs from 'dayjs'
+
+
+async function createVideoRows(videos: any) {
+  const newVideoRows = [];
+    for (const video of videos) {
+      const videoId = video.id.videoId;
+      const response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/videos?part=statistics%2C%20snippet&id=${videoId}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
+      );
+      const views = response.data.items[0].statistics.viewCount;
+      const snippet = video.snippet;
+      const title = snippet.title;
+      const timestamp = dayjs(snippet.publishedAt).format('DD/MM/YYYY');
+      const channel = snippet.channelTitle;
+      const description = snippet.description;
+      const image = snippet.thumbnails.medium.url;
+
+      newVideoRows.push({
+        videoId,
+        title,
+        image,
+        views,
+        timestamp,
+        channel,
+        description,
+      });
+    }
+  return newVideoRows;
+
+}
+
+
+export default function useVideosBySearchQuery(searchQuery: string): {
+  videos: any[],
+  loading: boolean,
+  error: boolean,
+  } {
+  const [videoCards, setVideoCards] = useState<any[]>([]);
+  const [Error, setError] = useState(false);
+
+  useEffect(() => {
+    axios
+        .get(
+          `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=9&type=video&q=${searchQuery}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
+        )
+        .then((response) => {
+          console.log({response})
+          createVideoRows(response?.data?.items).then((res) => setVideoCards(res) )
+          setError(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setError(error);
+        });
+
+  },[searchQuery])
+
+  return {
+    videos: videoCards,
+    loading: !Error && !videoCards,
+    error: Error,
+  };
+}
--- pages/search/[searchQuery].tsx
+++ pages/search/[searchQuery].tsx
@@ -1,71 +1,25 @@
+import React from 'react';
 import { useRouter } from 'next/router';
 import { Box, Text } from '@chakra-ui/react';
 import Layout from '../../components/Layout';
-import React, { useState, useEffect } from 'react';
 import VideoRow from '../../components/VideoRow';
-import axios from 'axios';
-import dayjs from 'dayjs';
 import LoadingSpinner from '../../components/LoadingSpinner';
+import useVideosBySearchQuery from '../../data/videosBySearchQuery';
 
 export default function SearchResults() {
   const router = useRouter();
   const searchQuery = router.query.searchQuery;
-  const [videoRows, setVideoRows] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=9&type=video&q=${searchQuery}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
-      )
-      .then((response) => {
-        createVideoRows(response.data['items']);
-        setIsError(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setIsError(true);
-        setIsLoading(false);
-      });
-  }, [searchQuery]);
-
-  async function createVideoRows(videos: any) {
-    let newVideoRows = [];
-    for (const video of videos) {
-      const videoId = video.id.videoId;
-      const response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/videos?part=statistics%2C%20snippet&id=${videoId}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
-      );
-      const views = response.data.items[0].statistics.viewCount;
-      const snippet = video.snippet;
-      const title = snippet.title;
-      const timestamp = dayjs(snippet.publishedAt).format('DD/MM/YYYY');
-      const channel = snippet.channelTitle;
-      const description = snippet.description;
-      const image = snippet.thumbnails.medium.url;
-
-      newVideoRows.push({
-        videoId,
-        title,
-        image,
-        views,
-        timestamp,
-        channel,
-        description,
-      });
-    }
-    setVideoRows(newVideoRows);
-    setIsLoading(false);
-  }
+  const { videos, loading, error } = useVideosBySearchQuery(
+    searchQuery as string,
+  );
 
   return (
     <Layout>
       <Box w="full" overflowY="scroll" overflowX="hidden" p="4">
-        {isLoading ? <LoadingSpinner /> : null}
-        {isError ? <Text m={1}>No Results found!</Text> : null}
+        {loading ? <LoadingSpinner /> : null}
+        {error ? <Text m={1}>No Results found!</Text> : null}
 
-        {videoRows.map((item) => {
+        {videos.map((item) => {
           return (
             <VideoRow
               id={item.videoId}
